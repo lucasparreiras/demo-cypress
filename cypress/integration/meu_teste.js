8a5a95fb2ee402835a7c5251e686742dda145242
Lucas Parreiras <lucasacparreiras@gmail.com>
2021-11-21 17:18:51 -0300

    Testando o roteiro do TP02 - Teste end-to-end da disciplina engenharia de software DCC UFMG

---
+++ cypress/integration/meu_teste.js
@@ -0,0 +1,44 @@
+// meu_teste.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe('Meu primeiro teste', () => {
+    it('Não faz nada', () => {
+      expect(true).to.equal(true)
+    })
+  })
+
+  describe('Teste End-to-End', () => {
+    it('Meu Primeiro Teste', () => {
+        // Abre o site
+        cy.visit('http://localhost:5000/')
+        
+        // Verifica se existe o livro desejado
+        cy.get('[data-id=3]').should('contain.text', 'Design Patterns')
+        
+        // Calcula o frete
+        cy.get('[data-id=3]').within(() => {
+           cy.get('input').type('10000-000')
+           cy.contains('Calcular Frete').click().then
+           cy.wait(2000)
+        })
+        cy.get('.swal-text').contains('O frete é: R$')
+
+        // Fecha o pop-up com o preço do frete
+        cy.get('.swal-button').click()
+
+        // Realizar compra
+        cy.get('[data-id=3]').within(() => {
+          cy.contains('Comprar').click().then
+          cy.wait(2000)
+       })
+       cy.get('.swal-text').contains('Sua compra foi realizada com sucesso')
+
+       // Fecha o pop-up de confirmação de compra
+       cy.get('.swal-button').click()
+
+    })
+  })
